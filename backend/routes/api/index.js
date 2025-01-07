eedb426815a87a84e01d2e1e54a3251a9fc3c847
Togar-GitHub <togar_unix@Togar-Laptop>
2025-01-07 08:33:11 -0800

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,3 +1,37 @@
 const router = require('express').Router();
 
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+
+// list of const ...Router = require('./component.js);
+
+router.use(restoreUser);
+
+// list of router.use('/component', ...Router);
+
+// GET /api/restore-user
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// GET /api/require-auth
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get('/test', requireAuth, (req, res) => {
+  res.json ({ message: 'success' })
+});
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 module.exports = router;
